9216b1633f625aab309dea61642048d7724158b8
Prostagma <dlinchaki@mail.ru>
2023-06-12 15:35:28 +0300

    добавлена возможность выбрать рабочую зону

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,253 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.Location = new System.Drawing.Point(458, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(850, 609);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(346, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 48);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Старт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Controls.Add(this.radioButton3);
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(328, 82);
+            this.panel1.TabIndex = 6;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Enabled = false;
+            this.panel2.Location = new System.Drawing.Point(157, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(166, 72);
+            this.panel2.TabIndex = 9;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 47);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(164, 22);
+            this.textBox1.TabIndex = 8;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(0, 1);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(164, 44);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Выбрать файл";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(3, 3);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(105, 20);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Веб-камера";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(3, 29);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(110, 20);
+            this.radioButton3.TabIndex = 4;
+            this.radioButton3.Text = "Фотография";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(3, 55);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(69, 20);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.Text = "Видео";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.Visible = false;
+            // 
+            // panel3
+            // 
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Controls.Add(this.trackBar2);
+            this.panel3.Controls.Add(this.trackBar1);
+            this.panel3.Location = new System.Drawing.Point(12, 116);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(244, 105);
+            this.panel3.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 16);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Наклон";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 16);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Высота";
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(3, 62);
+            this.trackBar2.Maximum = 100;
+            this.trackBar2.Minimum = -100;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(236, 56);
+            this.trackBar2.TabIndex = 11;
+            this.trackBar2.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(5, 16);
+            this.trackBar1.Maximum = 175;
+            this.trackBar1.Minimum = -75;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(236, 56);
+            this.trackBar1.TabIndex = 10;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(198, 16);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Изменение рабочей области";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox2.Location = new System.Drawing.Point(12, 240);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(425, 295);
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 224);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 16);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Рабочая зона";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1324, 629);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Линчак ЛР5";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,22 +1,148 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using OpenCvSharp;
+﻿using OpenCvSharp;
 using OpenCvSharp.Extensions;
+using System;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+using Point = OpenCvSharp.Point;
+using Size = OpenCvSharp.Size;
 
 namespace Lab5
 {
     public partial class Form1 : Form
     {
+        bool runVideo;
+        VideoCapture capture;
+        Mat matInput,matWorkZone;
+        Thread cameraThread;
+        readonly Size sizeObject = new Size(640, 480);
+        string pathToFile;
+        readonly Point2f[] pointWorkZoneElite = new Point2f[] { new Point(100, 400), new Point(200, 180), new Point(440, 180), new Point(540, 400) };
+        Point2f[] sizeMatrixPoints = new Point2f[4] { new Point(0,240), new Point(0, 0), new Point(320, 0), new Point(320, 240)};
+        Point2f[] pointWorkZone = new Point2f[4];
+        Scalar scalarZone;
         public Form1()
         {
             InitializeComponent();
+            matWorkZone = new Mat();
+            for (byte i = 0; i < 4; i++)
+            {
+                pointWorkZone[i] = pointWorkZoneElite[i];
+            }
+            scalarZone = new Scalar(0, 0, 255);
+        }
+        private void DisposeVideo()
+        {
+            pictureBox1.Image = null;
+            pictureBox2.Image = null;
+            if (cameraThread != null && cameraThread.IsAlive) cameraThread.Abort();
+            matInput?.Dispose();
+            capture?.Dispose();
+        }
+        private void DrawLinesAtMat(ref Mat mat, Point2f[] points, Scalar scalar)
+        {
+            mat.Line(points[0].ToPoint(), points[1].ToPoint(), scalar);
+            mat.Line(points[1].ToPoint(), points[2].ToPoint(), scalar);
+            mat.Line(points[2].ToPoint(), points[3].ToPoint(), scalar);
+            mat.Line(points[0].ToPoint(), points[3].ToPoint(), scalar);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (runVideo)
+            {
+                runVideo = false;
+                DisposeVideo();
+                button1.Text = "Старт";
+            }
+            else
+            {
+                runVideo = true;
+                matInput = new Mat();
+
+                if (radioButton1.Checked)
+                {
+                    capture = new VideoCapture(0)
+                    {
+                        FrameHeight = sizeObject.Height,
+                        FrameWidth = sizeObject.Width,
+                        AutoFocus = true
+                    };
+                }
+                cameraThread = new Thread(new ThreadStart(CaptureCameraCallback));
+                cameraThread.Start();
+                button1.Text = "Стоп";
+            }
+        }
+        private void CaptureCameraCallback()
+        {
+            while (runVideo)
+            {
+                matInput = radioButton1.Checked ? capture.RetrieveMat() : new Mat(pathToFile).Resize(sizeObject);
+                var matWithZone = matInput.Clone();
+                DrawLinesAtMat(ref matWithZone, pointWorkZone, scalarZone);
+
+                var matrix = Cv2.GetPerspectiveTransform(pointWorkZone, sizeMatrixPoints);
+                
+                Cv2.WarpPerspective(matInput, matWorkZone, matrix, new Size(320,240));
+
+                Invoke(new Action(() =>
+                {
+                    pictureBox1.Image = BitmapConverter.ToBitmap(matWithZone);
+                    pictureBox2.Image = BitmapConverter.ToBitmap(matWorkZone);
+                    GC.Collect();
+                    GC.WaitForPendingFinalizers();
+                }));
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog()
+            {
+                Multiselect = false,
+                Filter = "Мультимедия (*.bmp, *.jpg, *.png, *.mp4)| *.bmp; *.jpg; *.png; *.mp4"
+            };
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                var tempPath = file.FileName;
+                if (File.Exists(tempPath))
+                {
+                    pathToFile = tempPath;
+                    textBox1.Text = pathToFile;
+                }
+            }
+            file.Dispose();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DisposeVideo();
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            panel2.Enabled = true;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            panel2.Enabled = false;
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            for (byte i = 0; i < 4; i++)
+            {
+                pointWorkZone[i].Y = pointWorkZoneElite[i].Y - trackBar1.Value;
+            }
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+
+            pointWorkZone[1].X = pointWorkZoneElite[1].X + trackBar2.Value;
+            pointWorkZone[2].X = pointWorkZoneElite[2].X - trackBar2.Value;
+
         }
     }
 }
